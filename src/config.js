21ea0a7d6e423208af34273677aa3ee257ee70e8
Thomas Malt <thomas@malt.no>
2020-04-12 13:34:29 +0200

    created a config file with secrets

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const { version } = require('../package.json');
+
+module.exports = {
+  version,
+  users: {
+    // Add users here
+  },
+  keys: [
+    // add api keys here
+  ],
+  accounts: {
+    // add accounts you want to fetch here.
+  }
+};
