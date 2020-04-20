27d290116d7a7cabbbadc04ce33c905add8a1982
Thomas Malt <thomas@malt.no>
2020-04-21 00:22:29 +0200

    updating config

--- src/config.js
+++ src/config.js
@@ -7,8 +7,10 @@ module.exports = {
   },
   keys: [
     // add api keys here
+    'add_valid_apikey_here'
   ],
   accounts: {
     // add accounts you want to fetch here.
-  }
+  },
+  expenseKey: 'expense_key'
 };
